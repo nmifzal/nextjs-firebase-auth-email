1c07a4cae5a34ea2cddad2fa277b0d5240812fac
Mohamed Nashid Mifzal P <nashid@elixirbs.in>
2022-09-02 12:17:12 +0530

    env

--- config/firebase.ts
+++ config/firebase.ts
@@ -2,14 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwySG4r5iDhMcuMIIriL7EjlFt7dN5dkg",
-  authDomain: "elixr-crm.firebaseapp.com",
-  projectId: "elixr-crm",
-  storageBucket: "elixr-crm.appspot.com",
-  messagingSenderId: "1010812170338",
-  appId: "1:1010812170338:web:f01ccb5161f7a9a1a0e13b",
-  measurementId: "G-2BKKMJ2NMR",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
+
+console.log(firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
